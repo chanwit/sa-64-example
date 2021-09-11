5e1ba5655d57286d87c6be7ef9b31fc72c2f4007
Chanwit Kaewkasi <chanwit@gmail.com>
2021-09-11 15:45:52 +0700

    Add files via upload

--- backend/entity/setup.go
+++ backend/entity/setup.go
@@ -1,8 +1,11 @@
 package entity
 
 import (
-	"gorm.io/gorm"
+	"fmt"
+  "time"
+
 	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 var db *gorm.DB
@@ -12,13 +15,140 @@ func DB() *gorm.DB {
 }
 
 func SetupDatabase() {
-  database, err := gorm.Open(sqlite.Open("sa-64.db"), &gorm.Config{})
-  if err != nil {
-    panic("failed to connect database")
+	database, err := gorm.Open(sqlite.Open("sa-64.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	database.AutoMigrate(
+		&Video{},
+		&User{},
+		&Playlist{},
+		&Resolution{},
+		&WatchVideo{},
+	)
+
+	db = database
+
+	db.Model(&User{}).Create(&User{
+		Name:  "Chanwit",
+		Email: "chanwit@gmail.com",
+	})
+  db.Model(&User{}).Create(&User{
+    Name:  "Name",
+    Email: "name@example.com",
+  })
+
+	var chanwit User
+  var name User
+	db.Raw("SELECT * FROM users WHERE email = ?", "chanwit@gmail.com").Scan(&chanwit)
+  db.Raw("SELECT * FROM users WHERE email = ?", "name@example.com").Scan(&name)
+
+
+  // --- Video Data
+	saLecture4 := Video{
+		Name:  "SA Lecture 4",
+		Url:   "https://youtu.be/123",
+		Owner: chanwit,
+	}
+	db.Model(&Video{}).Create(&saLecture4)
+
+  howTo := Video{
+    Name:  "How to ...",
+    Url:   "https://youtu.be/456",
+    Owner: chanwit,
   }
+  db.Model(&Video{}).Create(&howTo)
 
-  // Migrate the schema
-  database.AutoMigrate(&User{})
 
-  db = database
+  helloWorld := Video{
+		Name:  "Hello World with C",
+		Url:   "https://youtu.be/789",
+		Owner: name,
+	}
+	db.Model(&Video{}).Create(&helloWorld)
+
+
+  // Resolution Data
+  res360p := Resolution{
+    Value: "360p",
+  }
+  db.Model(&Resolution{}).Create(&res360p)
+
+  res480p := Resolution{
+    Value: "480p",
+  }
+  db.Model(&Resolution{}).Create(&res480p)
+
+  res720p := Resolution{
+    Value: "720p",
+  }
+  db.Model(&Resolution{}).Create(&res720p)
+
+  watchedPlayListOfChanwit := Playlist{
+    Title: "Watched",
+    Owner: chanwit,
+  }
+  db.Model(&Playlist{}).Create(&watchedPlayListOfChanwit)
+
+	musicPlayListOfChanwit := Playlist{
+		Title: "Music",
+		Owner: chanwit,
+	}
+  db.Model(&Playlist{}).Create(&musicPlayListOfChanwit)
+
+  watchedPlayListOfName := Playlist{
+    Title: "Watched",
+    Owner: name,
+  }
+  db.Model(&Playlist{}).Create(&watchedPlayListOfName)
+
+
+  // watch 1
+	db.Model(&WatchVideo{}).Create(&WatchVideo{
+		Playlist:    watchedPlayListOfChanwit,
+		Video:       saLecture4,
+		WatchedTime: time.Now(),
+		Resolution:  res720p,
+	})
+	// watch 2
+	db.Model(&WatchVideo{}).Create(&WatchVideo{
+		Playlist:    watchedPlayListOfName,
+		Video:       helloWorld,
+		WatchedTime: time.Now(),
+		Resolution:  res480p,
+	})
+  // watch 3
+  db.Model(&WatchVideo{}).Create(&WatchVideo{
+    Playlist:    watchedPlayListOfChanwit,
+    Video:       helloWorld,
+    WatchedTime: time.Now(),
+    Resolution:  res720p,
+  })
+
+	//
+	// === Query
+	//
+
+	var target User
+	db.Model(&User{}).Find(&target, db.Where("email = ?", "chanwit@gmail.com"))
+
+	var watchedPlaylist Playlist
+	db.Model(&Playlist{}).Find(&watchedPlaylist, db.Where("title = ? and owner_id = ?", "Watched", target.ID))
+
+	var watchedList []*WatchVideo
+	db.Model(&WatchVideo{}).
+		Joins("Playlist").
+		Joins("Resolution").
+		Joins("Video").
+		Find(&watchedList, db.Where("playlist_id = ?", watchedPlaylist.ID))
+
+	for _, wl := range watchedList {
+		fmt.Printf("Watch Video: %v\n", wl.ID)
+		fmt.Printf("%v\n", wl.Playlist.Title)
+		fmt.Printf("%v\n", wl.Resolution.Value)
+		fmt.Printf("%v\n", wl.Video.Name)
+		fmt.Println("====")
+	}
 }
--- backend/entity/user.go
+++ backend/entity/user.go
@@ -1,15 +1,60 @@
 package entity
 
 import (
-  "gorm.io/gorm"
-  "time"
+	"gorm.io/gorm"
+	"time"
 )
 
 type User struct {
-  gorm.Model
-  FirstName    string
-  LastName     string
-  Email        string
-  Age          uint8
-  BirthDay     time.Time
+	gorm.Model
+	Name      string
+	Email     string     `gorm:"uniqueIndex"`
+  // 1 user เป็นเจ้าของได้หลาย video
+	Videos    []Video    `gorm:"foreignKey:OwnerID"`
+  // 1 user เป็นเจ้าของได้หลาย playlist
+	Playlists []Playlist `gorm:"foreignKey:OwnerID"`
+}
+
+type Video struct {
+	gorm.Model
+	Name        string
+	Url         string       `gorm:"uniqueIndex"`
+	// OwnerID ทำหน้าที่เป็น FK
+  OwnerID     *uint
+  // เป็นข้อมูล user เมื่อ join ตาราง
+	Owner       User
+	WatchVideos []WatchVideo `gorm:"foreignKey:VideoID"`
+}
+
+type Playlist struct {
+	gorm.Model
+	Title       string
+  // OwnerID ทำหน้าที่เป็น FK
+	OwnerID     *uint
+  // เป็นข้อมูล user เมื่อ join ตาราง
+	Owner       User
+	WatchVideos []WatchVideo `gorm:"foreignKey:PlaylistID"`
+}
+
+type Resolution struct {
+	gorm.Model
+	Value       string
+	WatchVideos []WatchVideo `gorm:"foreignKey:ResolutionID"`
+}
+
+type WatchVideo struct {
+	gorm.Model
+  WatchedTime  time.Time
+
+  // ResolutionID ทำหน้าที่เป็น FK
+  ResolutionID *uint
+  Resolution   Resolution
+
+  // PlaylistID ทำหน้าที่เป็น FK
+	PlaylistID   *uint
+	Playlist     Playlist
+
+  // VideoID ทำหน้าที่เป็น FK
+	VideoID      *uint
+	Video        Video
 }
