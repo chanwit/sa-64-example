661c40dcaa4510410eb5a968b53a6df395737a91
tanapon395 <tanapon395@gmail.com>
2021-10-10 14:08:35 +0700

    fix WatchedTime controller watch video func CreateWatchVideo

--- backend/controller/watch_video.go
+++ backend/controller/watch_video.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/chanwit/sa-64-example/entity"
 	"github.com/gin-gonic/gin"
@@ -10,39 +9,44 @@ import (
 
 // POST /watch_videos
 func CreateWatchVideo(c *gin.Context) {
-	now := time.Now()
 
 	var watchvideo entity.WatchVideo
 	var resolution entity.Resolution
 	var playlist entity.Playlist
 	var video entity.Video
 
+	// ผลลัพธ์ที่ได้จากขั้นตอนที่ 8 จะถูก bind เข้าตัวแปร watchVideo
 	if err := c.ShouldBindJSON(&watchvideo); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	if tx := entity.DB().Where("id = ?", watchvideo.ResolutionID).First(&resolution); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "resolution not found"})
-		return
-	}
-
-	if tx := entity.DB().Where("id = ?", watchvideo.PlaylistID).First(&playlist); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "playlist not found"})
-		return
-	}
-
+	// 9: ค้นหา video ด้วย id
 	if tx := entity.DB().Where("id = ?", watchvideo.VideoID).First(&video); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "video not found"})
 		return
 	}
 
-	wv := entity.WatchVideo{
-		WatchedTime: now,
-		Resolution:  resolution,
-		Video:       video,
-		Playlist:    playlist}
+	// 10: ค้นหา resolution ด้วย id
+	if tx := entity.DB().Where("id = ?", watchvideo.ResolutionID).First(&resolution); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "resolution not found"})
+		return
+	}
 
+	// 11: ค้นหา playlist ด้วย id
+	if tx := entity.DB().Where("id = ?", watchvideo.PlaylistID).First(&playlist); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "playlist not found"})
+		return
+	}
+	// 12: สร้าง WatchVideo
+	wv := entity.WatchVideo{
+		Resolution:  resolution,             // โยงความสัมพันธ์กับ Entity Resolution
+		Video:       video,                  // โยงความสัมพันธ์กับ Entity Video
+		Playlist:    playlist,               // โยงความสัมพันธ์กับ Entity Playlist
+		WatchedTime: watchvideo.WatchedTime, // ตั้งค่าฟิลด์ watchedTime
+	}
+
+	// 13: บันทึก
 	if err := entity.DB().Create(&wv).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
